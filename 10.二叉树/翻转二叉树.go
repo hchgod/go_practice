fc5c575b16a93e1e27c7e04b0f65c34ffb06237a
huangchanghao <1065449451@qq.com>
2024-04-06 23:46:49 +0800

    4.6

--- 10.二叉树/翻转二叉树.go
+++ 10.二叉树/翻转二叉树.go
@@ -3,7 +3,6 @@ package main
 // 使用队列层次遍历
 import (
 	"fmt"
-	
 )
 
 type TreeNode struct {
@@ -31,6 +30,7 @@ func create_tree(nums []int, index int) *TreeNode {
 }
 
 func reverse_tree(root *TreeNode) (node *TreeNode) {
+	//递归版本
 	if root == nil {
 		return nil
 	}
@@ -40,29 +40,65 @@ func reverse_tree(root *TreeNode) (node *TreeNode) {
 	return root
 }
 
+func reverse_tree2(root *TreeNode) *TreeNode {
+	//前序遍历   用栈实现
+	stack := []*TreeNode{}
+	node := root
+	for node != nil || len(stack) != 0 {
+		for node != nil {
+			stack = append(stack, node)
+			node.Left, node.Right = node.Right, node.Left
+			node = node.Left
+		}
+		if node == nil {
+			node = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+		}
+		node = node.Right
+	}
+	return root
+}
+
+func reverse_tree3(root *TreeNode) *TreeNode {
+	//中序遍历   用栈实现    不可实现
+	stack := []*TreeNode{}
+	node := root
+	for node != nil || stack != nil {
+		for node != nil {
+			stack = append(stack, node)
+			node = node.Left
+		}
+		node = stack[len(stack)-1]
+		node.Left,node.Right = node.Right,node.Left
+		stack = stack[:len(stack)-1]
+		node = node.Right
+	}
+	return root
+}
+
 func levelorder(root *TreeNode) (res [][]int) {
 	depth := 0
-	var order func(root *TreeNode,depth int)
-	order = func(root *TreeNode,depth int) {
+	var order func(root *TreeNode, depth int)
+	order = func(root *TreeNode, depth int) {
 		if root == nil {
-			return 
+			return
 		}
-		if len(res)	== depth{
+		if len(res) == depth {
 			res = append(res, []int{})
 		}
 		res[depth] = append(res[depth], root.Val)
-		order(root.Left,depth+1)
-		order(root.Right,depth+1)
+		order(root.Left, depth+1)
+		order(root.Right, depth+1)
 	}
-	order(root,depth)
+	order(root, depth)
 	// reverse(res)
-    return res
+	return res
 }
 
 func main() {
 	nums := []int{1, 2, 3, 4, 5, 6, 7}
 	node := create_tree(nums, 0)
-	node = reverse_tree(node)
+	node = reverse_tree3(node)
 	res := levelorder(node)
 	fmt.Print(res)
 }
